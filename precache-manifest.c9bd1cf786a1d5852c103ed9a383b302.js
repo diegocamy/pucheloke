664bcb4094dfc257a5394525fbea92bba6b26166
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-08 22:37:07 -0300

    Updates

---
+++ precache-manifest.c9bd1cf786a1d5852c103ed9a383b302.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4510b6b4b0c990a04ea3792cb48e4fe4",
+    "url": "/pucheloke/index.html"
+  },
+  {
+    "revision": "763c5e27b2e5807fb28f",
+    "url": "/pucheloke/static/css/main.8c5d7a7f.chunk.css"
+  },
+  {
+    "revision": "ed25274b1ae683c4ef12",
+    "url": "/pucheloke/static/js/2.5ef34e61.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pucheloke/static/js/2.5ef34e61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "763c5e27b2e5807fb28f",
+    "url": "/pucheloke/static/js/main.fa2c68cc.chunk.js"
+  },
+  {
+    "revision": "2f95c35dd93080e66f5e",
+    "url": "/pucheloke/static/js/runtime-main.916daa21.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/pucheloke/static/media/background.adbccee0.png"
+  },
+  {
+    "revision": "365edea8447db2b442746dc468be7d79",
+    "url": "/pucheloke/static/media/puche.365edea8.jpg"
+  }
+]);
